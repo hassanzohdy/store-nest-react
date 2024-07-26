dbfc516f3d886e548aabc118904e53d742a92870
ahmedmo2399 <ahmedabdelmoeen99@gmail.com>
2024-07-26 23:55:35 +0300

    img

---
+++ src/apps/front-office/design-system/components/ui/DealsOfTheDay/DealList.jsx
@@ -0,0 +1,13 @@
+import DealCard from './DealCard';
+
+const DealList = ({ deals }) => {
+  return (
+    <div className="flex flex-wrap -m-2 gap-4">
+      {deals.map((deal, index) => (
+        <DealCard key={index} {...deal} />
+      ))}
+    </div>
+  );
+};
+
+export default DealList;
--- src/apps/front-office/home/pages/HomePage/HomePage.tsx
+++ src/apps/front-office/home/pages/HomePage/HomePage.tsx
@@ -1,151 +1,76 @@
 import { trans } from "@mongez/localization";
 import Helmet from "@mongez/react-helmet";
-import { FaAngleRight, FaShoppingCart } from "react-icons/fa";
-import { FcRating } from "react-icons/fc";
+import DealList from "../../../design-system/components/ui/DealsOfTheDay/DealList";
+import Header from "../../../design-system/components/ui/DealsOfTheDay/Header";
+import SectionWrapper from "../../../design-system/components/ui/DealsOfTheDay/SectionWrapper";
 
 export default function HomePage() {
+  const deals = [
+    {
+      imageSrc: "/img/banner-5.png",
+      title: "Seeds of Change Organic Quinoa, Brown, & Red Rice",
+      rating: "4.0",
+      by: "NestFood",
+      price: "32.85",
+      oldPrice: "33.8",
+      timeCards: [
+        { time: "2", type: "Days" },
+        { time: "14", type: "Hours" },
+        { time: "20", type: "Mins" },
+        { time: "30", type: "Secs" },
+      ],
+    },
+    {
+      imageSrc: "/img/banner-6.png",
+      title: "Perdue Simply Smart Organics Gluten Free",
+      rating: "4.5",
+      by: "Old El Paso",
+      price: "24.85",
+      oldPrice: "26.8",
+      timeCards: [
+        { time: "1", type: "Days" },
+        { time: "10", type: "Hours" },
+        { time: "30", type: "Mins" },
+        { time: "45", type: "Secs" },
+      ],
+    },
+    {
+      imageSrc: "/img/banner-7.png",
+      title: "Signature Wood-Fired Mushroom and Caramelized",
+      rating: "3.0",
+      by: "Progresso",
+      price: "12.85",
+      oldPrice: "13.8",
+      timeCards: [
+        { time: "3", type: "Days" },
+        { time: "6", type: "Hours" },
+        { time: "45", type: "Mins" },
+        { time: "50", type: "Secs" },
+      ],
+    },
+    {
+      imageSrc: "/img/banner-8.png",
+      title: "Simply Lemonade with Raspberry Juice",
+      rating: "3.0",
+      by: "Yoplait",
+      price: "15.85",
+      oldPrice: "16.8",
+      timeCards: [
+        { time: "0", type: "Days" },
+        { time: "8", type: "Hours" },
+        { time: "50", type: "Mins" },
+        { time: "10", type: "Secs" },
+      ],
+    },
+  ];
+
   return (
     <>
       <Helmet title={trans("home")} appendAppName={false} />
-      <section className="py-10 pb-5">
-        <div className="container mx-auto pr-4 pl-4">
-          <div className="flex justify-between items-center p-4 ">
-            <h3 className="font-quicksand text-slate-600 leading-tight mr-8 text-2xl font-bold mb-4">
-              Deals Of The Day
-            </h3>
-            <a
-              className=" flex items-center justify-center text-base  text-gray-500 transition duration-300  hover:text-teal-500"
-              href="#">
-              All Deals
-              <FaAngleRight className="ml-2 w-5" />
-            </a>
-          </div>
-        </div>
-        <div className="flex flex-wrap -m-2">
-          <div className="w-full sm:w-1/2 lg:w-1/4 px-4 mb-6">
-            <div className="border-0 pb-12 relative bg-white rounded-2xl overflow-hidden transition duration-200">
-              <div className="p-0 relative z-0">
-                <div className="relative overflow-hidden rounded-3xl">
-                  <a className="overflow-hidden block" href="#">
-                    <img src="/public/img/banner-5.png" alt="" />
-                  </a>
-                </div>
-                <div className="absolute top-0 w-full text-center">
-                  <div className="flex space-x-4">
-                    <span></span>
-                    <span>2</span>
-                    <span>3</span>
-                    <span>4</span>
-                  </div>
-                </div>
-              </div>
-
-              <div className="relative mt-[-90px] z-[3] p-0 max-w-[94%] mx-auto transition-top duration-500 hover:mt-[-100px] ">
-                <div className="mx-auto bg-white rounded-xl shadow-md pt-6 pb-6 pr-8 pl-7">
-                  <h2 className="font-quicksand text-sm font-bold mb-1 transition duration-300 hover:text-teal-500">
-                    <a href="#">
-                      Seeds of Change Organic Quinoa, Brown, & Red Rice
-                    </a>
-                  </h2>
-                  <div className="mt-2">
-                    <div className="flex items-center space-x-0">
-                      <div className="h-4 w-6 rounded-md">
-                        <FcRating />
-                      </div>
-                      <span className="text-sm ml-0 text-gray-500">(4.0)</span>
-                    </div>
-                  </div>
-                  <div className="text-base font-normal leading-6 font-lato mt-1">
-                    <span className="text-gray-500 pr-1 mt-1">BY</span>
-                    <a
-                      className="text-teal-500 transition duration-300 hover:text-orange-400"
-                      href="#">
-                      NestFood
-                    </a>
-                  </div>
-                  <div className="flex justify-between mt-4 font-bold">
-                    <div className="pt-1">
-                      <span className="text-teal-500 text-lg">$32.85</span>
-                      <span className="text-base text-gray-500 ml-2 line-through">
-                        $33.8
-                      </span>
-                    </div>
-                    <div>
-                      <a
-                        className="flex items-center py-1.5 px-5 rounded-md bg-teal-200 font-bold text-base text-teal-500 duration-300 hover:text-white hover:bg-green-600"
-                        href="#">
-                        <FaShoppingCart className="mr-1" />
-                        Add
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          {/* <div className="w-full sm:w-1/2 lg:w-1/4 px-4 mb-6">
-            <div className="border-0 pb-12 relative bg-white rounded-2xl overflow-hidden transition duration-200">
-              <div className="p-0 relative z-0">
-                <div className="relative overflow-hidden rounded-3xl">
-                  <a className="overflow-hidden block" href="#">
-                    <img src="public/img/banner-5.png" alt="" />
-                  </a>
-                </div>
-              </div>
-              <div className="absolute top-0 w-full text-center">
-                <div className="flex space-x-4">
-                  <span></span>
-                  <span>2</span>
-                  <span>3</span>
-                  <span>4</span>
-                </div>
-              </div>
-              <div className="relative mt-[-90px] z-[3] p-0 max-w-[94%] mx-auto hover:mt-[-100px] transition duration-500">
-                <div className="mx-auto bg-white rounded-xl shadow-md pt-6 pb-6 pr-8 pl-7">
-                  <h2 className="font-quicksand text-sm font-bold mb-1 transition duration-300 hover:text-teal-500">
-                    <a href="#">
-                      Seeds of Change Organic Quinoa, Brown, & Red Rice
-                    </a>
-                  </h2>
-                  <div className="mt-2">
-                    <div className="flex items-center space-x-0">
-                      <div className="h-4 w-6 rounded-md">
-                        <FcRating />
-                      </div>
-                      <span className="text-sm ml-0 text-gray-500">(4.0)</span>
-                    </div>
-                  </div>
-                  <div className="text-base font-normal leading-6 font-lato mt-1">
-                    <span className="text-gray-500 pr-1 mt-1">BY</span>
-                    <a
-                      className="text-teal-500 transition duration-300 hover:text-orange-400"
-                      href="#">
-                      NestFood
-                    </a>
-                  </div>
-                  <div className="flex justify-between mt-4 font-bold">
-                    <div className="pt-1">
-                      <span className="text-teal-500 text-lg">$32.85</span>
-                      <span className="text-base text-gray-500 ml-2 line-through">
-                        $33.8
-                      </span>
-                    </div>
-                    <div>
-                      <a
-                        className="flex items-center py-1.5 px-5 rounded-md bg-teal-200 font-bold text-base text-teal-500 duration-300 hover:text-white hover:bg-green-600"
-                        href="#">
-                        <FaShoppingCart className="mr-1" />
-                        Add
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div> */}
-        </div>
-      </section>
+      <SectionWrapper>
+        <Header title="Deals Of The Day" linkText="All Deals" linkHref="#" />
+        <DealList deals={deals} />
+      </SectionWrapper>
     </>
   );
 }
