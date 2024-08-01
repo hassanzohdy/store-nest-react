a2c0155c4405e1a053135a76f77b5543b973c13a
MennaBashir <mennabashir00@gmail.com>
2024-08-01 22:35:08 +0200

    fix:resolve_conflict

--- src/apps/front-office/home/pages/HomePage/sections/DealsDayPart2/DealsDayPart2.tsx
+++ src/apps/front-office/home/pages/HomePage/sections/DealsDayPart2/DealsDayPart2.tsx
@@ -5,7 +5,7 @@ import TrendingProducts from "./TrendingProducts";
 
 const DealsDayPart2 = () => {
   return (
-    <section className="container my-24 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-9 h-[800px] small-to-medium:h-[3670px] md:h-[420px]   overflow-hidden ">
+    <section className="container my-24 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-9 h-[800px] small-to-medium:h-[3680px] md:h-[420px]   overflow-hidden ">
       <TopSelling />
       <TrendingProducts />
       <RecentlyAdded />
